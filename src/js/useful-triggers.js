ada4154e9da474b8fdf871d5ba8ee87f0bccbff2
Maurice van Creij <maurice.vancreij@webqem.com>
2017-06-09 09:00:12 +1000

    Configure dependencies in build script

--- src/js/useful-triggers.js
+++ src/js/useful-triggers.js
@@ -142,6 +142,7 @@ useful.Triggers.prototype.Navigation = function () {
 	"use strict";
 	this.context = null;
 	this.config = {};
+	this.previous = -1;
 
 	// METHODS
 
@@ -195,24 +196,37 @@ useful.Triggers.prototype.Navigation = function () {
 	// EVENTS
 
 	this.onNavigated = function (hash, evt) {
+		// if the hash is invalid give up
+		if (!/^#/.test(hash)) { return true; }
 		// stop the actual click
 		if (evt) { evt.preventDefault(); }
 		// cancel any previous scrolling
 		clearInterval(this.scrollInterval);
-		// scroll to the required position
+		// get the involved elements
 		var _this = this,
 			target = document.querySelector(hash),
 			navigation = document.querySelector(this.config.nav),
 			scroller = this.config.element;
+		// scroll to the required position
 		this.scrollInterval = setInterval(function () {
 			// find the target position
 			var destination = target.offsetTop - navigation.offsetHeight;
 			var position = useful.positions.document(scroller);
-			// if the position is close enough to the destination
-			if (Math.abs(destination - position.y) < 10) {
+			//console.log('navigate to:', destination, 'from', position.y);
+			// check if the scrolling has been stuck
+			if (position.y === _this.previous) { _this.stuck += 1; }
+			// if the position is close enough to the destination or stuck
+			if (Math.abs(destination - position.y) < 10 || _this.stuck > 2) {
+				// clear the current position
+				_this.stuck = -1;
+				_this.previous = -1;
 				// cancel the interval
 				clearInterval(_this.scrollInterval);
+				// set the hash
+				window.location.hash = hash;
 			} else {
+				// store the current position
+				_this.previous = position.y;
 				// jump closer to the destination
 				scroller.scrollTo(position.x, (destination - position.y) / 10 + position.y);
 			}
